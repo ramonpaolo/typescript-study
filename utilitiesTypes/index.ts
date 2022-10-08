38ea0421c356ba53fb8b9df9dbc1513dbf972336
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2022-10-08 16:20:05 -0300

    feat: add example of utilitie Record

--- utilitiesTypes/index.ts
+++ utilitiesTypes/index.ts
@@ -34,3 +34,19 @@ interface ITodo3 {
 const updateTodo3: Readonly<ITodo3> = {title: 'title readonly', describe: 'describe readonly'}
 
 updateTodo3.title = 'try update title';
+
+// ------------------------------------------- Record
+
+interface ITodo4 {
+    title: string
+    describe: string
+}
+
+type Name = 'Ramon' | 'Rodolfo'
+
+const updateTodo4: Record<Name, ITodo4> = {
+    Ramon: {title: 'Title of Ramon', describe: 'describe of Ramon'},
+    Rodolfo: {title: 'Title of Rodolfo', describe: 'describe of Rodolfo'}
+}
+
+updateTodo4.Ramon.describe = 'try update title of Ramon';
