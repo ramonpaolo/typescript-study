012e65dcea7328873b47ec6a8c375af9551c6853
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2022-10-08 16:13:39 -0300

    feat: add example of utilitie Required

--- utilitiesTypes/index.ts
+++ utilitiesTypes/index.ts
@@ -9,3 +9,17 @@ const updateTodo = (todo: ITodo, someFields: Partial<ITodo>) => {
 }
 
 updateTodo({title: 'eae', describe: 'Opa'}, {title: 's'})
+
+// ------------------------------------------- Required
+
+interface ITodo2 {
+    title?: string
+    describe?: string
+}
+
+const updateTodo2 = (todo: ITodo2, someFields: Required<ITodo2>) => {
+    console.log(todo)
+    console.log(someFields)
+}
+
+updateTodo2({title: 'eae', describe: 'Opa'}, {title: 's', describe: 'required'})
