06c8e941a4bbaed119473a210810232dfe9019c8
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2022-10-08 16:16:03 -0300

    feat: add example of utilitie Readonly

--- utilitiesTypes/index.ts
+++ utilitiesTypes/index.ts
@@ -23,3 +23,14 @@ const updateTodo2 = (todo: ITodo2, someFields: Required<ITodo2>) => {
 }
 
 updateTodo2({title: 'eae', describe: 'Opa'}, {title: 's', describe: 'required'})
+
+// ------------------------------------------- Readonly
+
+interface ITodo3 {
+    title: string
+    describe: string
+}
+
+const updateTodo3: Readonly<ITodo3> = {title: 'title readonly', describe: 'describe readonly'}
+
+updateTodo3.title = 'try update title';
